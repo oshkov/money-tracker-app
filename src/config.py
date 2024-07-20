0bc77ef911d52c7e1a4e333a56b738d120c65a90
Alexander Oshkov <sasha.oshkov03@gmail.com>
2024-07-20 22:35:30 +0300

    Сделана аутентификация с помощью FastAPI Users

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+SECRET_AUTH = os.environ.get('SECRET_AUTH')
